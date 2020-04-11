b961317994f9170de53e7be20df92af6db4c270c
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-04-12 00:11:31 +0200

    Logging with LOG4j

---
+++ src/main/java/pkg/Main.java
@@ -0,0 +1,24 @@
+package pkg;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    private static final Logger logger = LogManager.getLogger("Main");
+    public static void main(String[] args) {
+        final Level VIOLATION = Level.forName("VIOLATION", 150); // szint a karantén szabályok megsértésének naplózásához
+        final Level REQUEST = Level.forName("REQUEST", 350); // szint kérelmek naplózásához
+        int n = Integer.parseInt(args[0]);
+        System.out.println(n);
+        for (int i = 1; i <= n; i++) {
+            logger.log(VIOLATION, "Zöld erdőben jártam,");
+            logger.info("Kék ibolyát láttam,");
+            logger.warn("El akart hervadni,");
+            logger.log(REQUEST, "Szabad-e locsolni?");
+            try {
+                Thread.sleep(10); // egy kis késleltetés
+            } catch (InterruptedException e) {}
+        }
+    }
+}
